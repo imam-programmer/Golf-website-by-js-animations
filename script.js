7861fd959a6a632c8773f9f3182c62aadd2b6be4
imam-programmer <imamprogrammer09@gmail.com>
2026-06-10 23:13:49 +0600

    cursor animation & banner animation done

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let crsr=document.querySelector("#cursor")
+let blur=document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",(dets)=>{
+crsr.style.left=dets.x+"px";
+crsr.style.top=dets.y+"px"
+blur.style.left=dets.x-200+"px";
+blur.style.top=dets.y-200+"px"
+})
+
+
+
+
+
+
+
 gsap.to("#nav",{
     backgroundColor:"#000",
     height:"100px",
@@ -8,6 +23,16 @@ gsap.to("#nav",{
         scroller:"body",
         start:"top -10px",
         end:"top 11%",
+        scrub:2
+    }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        start:"top -25%",
+        end:"top -70%",
         scrub:2
     }
 })
